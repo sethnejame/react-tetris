53a089b998b76ad1990c2a06a6c8cafd22f14c3a
Seth NeJame <sethnejame@gmail.com>
2019-08-18 18:40:25 +0200

    Building gameHelper, setting up Stage component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,10 +2,11 @@ import React from "react";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { createStage } from "../gameHelpers";
 
 const Tetris = ({ type }) => (
   <div>
-    <Stage />
+    <Stage stage={createStage()} />
     <aside>
       <Display text="Score" />
       <Display text="Rows" />
