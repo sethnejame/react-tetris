46f36a19e75dc5763570486502f0ffca4b3840b3
Seth NeJame <sethnejame@gmail.com>
2019-08-18 18:05:18 +0200

    Scaffolding game components in new component folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import Tetris from "./components/Tetris";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello world! </h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Tetris />
+  </div>
+);
 
 export default App;
