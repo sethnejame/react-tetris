312967947627daa57acfe28153091a31f38d292f
seth@mondido <sethnejame>
2019-09-29 15:26:45 +0200

    Adding styled tetris for blocks and styled tetris wrapper for bg @ main

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -4,16 +4,22 @@ import Display from "./Display";
 import StartButton from "./StartButton";
 import { createStage } from "../gameHelpers";
 
-const Tetris = ({ type }) => (
-  <div>
-    <Stage stage={createStage()} />
-    <aside>
-      <Display text="Score" />
-      <Display text="Rows" />
-      <Display text="Level" />
-    </aside>
-    <StartButton />
-  </div>
-);
+const Tetris = ({ type }) => {
+  return (
+    <StyledTetrisWrapper>
+      <StyledTetris>
+      <Stage stage={createStage()} />
+      <aside>
+        <div>
+          <Display text="Score" />
+          <Display text="Rows" />
+          <Display text="Level" />
+        </div>
+        <StartButton />
+      </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
+};
 
 export default Tetris;
