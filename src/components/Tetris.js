01576b1318d22429c54270300a4ec05fe2f14e35
seth@mondido <sethnejame>
2019-09-30 19:12:41 +0200

    Adding custom hooks to main Tetris component, wiring up, adding conditional display for gameOver @ display board

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Components
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
-import { createStage } from "../gameHelpers";
-import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris"
+
+// Custom Hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+
+// Styled Components
+import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
 
 const Tetris = ({ type }) => {
-  console.log(createStage())
+  const [dropTime, setDropTime] = useState(null); // toggle the speed of the dropping tetrominos here
+  const [gameOver, setGameOver] = useStage(false); // toggle true/false for game status
+  const [player] = usePlayer(); // pull in player object from custom hook
+  const [stage, setStage] = useStage(); // pull in stage object from custom hook
+
+  console.log("re-render");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
+        <Stage stage={stage} />
+        <aside>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over!" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
+          <StartButton />
+        </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
   );
